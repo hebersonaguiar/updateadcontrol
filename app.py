03b2d91230d53e8f9a05c853986d6bf5b698eff7
Heberson Rocha Aguiar <haguiar.hepta@epl.gov.br>
2020-02-18 16:53:12 -0300

    Alteracoes app.py

--- app.py
+++ app.py
@@ -227,13 +227,13 @@ def download():
                 item[0],
                 item[1],
                 item[2],
-                item[3],
-                item[4],
-                item[5],
-                item[6],
-				item[7],
-                item[8],
-                item[9].isoformat()  # format datetime as string
+                item[3]
+                # item[4],
+                # item[5],
+                # item[6],
+				# item[7],
+                # item[8],
+                # item[9].isoformat()  # format datetime as string
             ))
             yield data.getvalue()
             data.seek(0)
